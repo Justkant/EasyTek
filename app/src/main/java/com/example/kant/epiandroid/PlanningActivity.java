e178cfc4aec01a5061b21b94b4fbfa5e32e5e209
Justkant <quent92100@gmail.com>
2015-01-30 21:55:13 +0100

    start planning changes

--- app/src/main/java/com/example/kant/epiandroid/PlanningActivity.java
+++ app/src/main/java/com/example/kant/epiandroid/PlanningActivity.java
@@ -2,16 +2,14 @@ package com.example.kant.epiandroid;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.view.ViewPager;
 
-import com.example.kant.epiandroid.Planning.PlanningItemAdapter;
-import com.example.kant.epiandroid.Planning.PlanningItemData;
+import com.example.kant.epiandroid.Tabs.SlidingTabLayout;
+import com.example.kant.epiandroid.Tabs.SlidingTabPagerAdapter;
 
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
+public class PlanningActivity extends BaseActivity {
 
-public class PlanningActivity extends BaseActivity implements PlanningItemAdapter.ClickListener {
+    private ViewPager mPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,39 +19,17 @@ public class PlanningActivity extends BaseActivity implements PlanningItemAdapte
         getActionBarToolbar().setTitle(R.string.title_activity_planning);
         setSupportActionBar(getActionBarToolbar());
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.cal_item_recycler_view);
-        recyclerView.setHasFixedSize(true);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        SlidingTabLayout mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-
-        ArrayList<PlanningItemData> data = new ArrayList<PlanningItemData>();
-        for (int i = 0; i < 10; i++) {
-            PlanningItemData newData = new PlanningItemData();
-            newData.title = "Title" + i;
-            newData.dateStart = new GregorianCalendar(2015, 01, 29, 8 + i, 0);
-            newData.dateEnd = new GregorianCalendar(2015, 01, 29, 9 + i, 0);
-            newData.description = "Sunday.. Everyday!";
-
-            newData.isFirstOfTheDay = i == 0;
-
-            data.add(newData);
-        }
-
-        PlanningItemAdapter adapter = new PlanningItemAdapter(this, data);
-        recyclerView.setAdapter(adapter);
-    }
-
-    @Override
-    public void itemClicked(int position) {
-        super.itemClicked(position);
+        mPager.setAdapter(new SlidingTabPagerAdapter(getSupportFragmentManager(), this));
+        mTabs.setViewPager(mPager);
     }
 
     @Override
     protected int getSelfNavDrawerItem() {
         return PLANNING_ID;
     }
-
 
     @Override
     protected void onResume() {
